af3fe0fc357150b6ec6bb11141b1acbaabe9758e
JoaoGSRocha <joaogsrocha@gmail.com>
2018-02-13 14:10:17 +0000

    [Add] Triggered Sound at certain System.Time - Adds PlayAlarm.cs; - PlayAlarm triggers at certain hour and minute; - Adds Audio Source & UI Canvas & Text;
    Keywords: Add, PlayAlarm, Script, Audio Source, UI, Canvas, Text
    Importance: Critical

---
+++ Assets/PlayAlarm.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine.Events;
+
+[RequireComponent(typeof(AudioClip))]
+public class PlayAlarm : MonoBehaviour {
+    [SerializeField]
+    private AudioClip alarmSound;
+    [SerializeField]
+    private int hour, minutes;
+    private AudioSource sound { get { return GetComponent<AudioSource>(); } }
+    [SerializeField]
+    private Text timeText;
+
+	void Start () {
+        timeText = GetComponent<UnityEngine.UI.Text>();
+        sound.clip = alarmSound;
+        sound.playOnAwake = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        System.DateTime time = System.DateTime.Now;
+        timeText.text = time.ToString("hh:mm");
+        if (time.Hour == hour && time.Minute == minutes && !sound.isPlaying)
+        {
+            Debug.Log("playing");
+            sound.PlayOneShot(alarmSound);
+        }
+	}
+}
